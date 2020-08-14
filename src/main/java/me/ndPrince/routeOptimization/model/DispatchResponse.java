c3b186f1b94e65c8f4c23109771b307befbe262b
YOUNGSEOK LEE <astonisher88@gmail.com>
2020-08-14 15:30:40 -0400

    completed

--- src/main/java/me/ndPrince/routeOptimization/model/DispatchResponse.java
+++ src/main/java/me/ndPrince/routeOptimization/model/DispatchResponse.java
@@ -1,11 +1,19 @@
 package me.ndPrince.routeOptimization.model;
 
+import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
+@Builder
 public class DispatchResponse {
 
+    @Builder.Default
+    @NotNull
+    protected String id;
+
     private List<VehicleScheduleResponse> vehicleSchedules;
+    private List<Double> dummyList;
 }
--- src/test/java/me/ndPrince/routeOptimization/DispatchEndToEndTest.java
+++ src/test/java/me/ndPrince/routeOptimization/DispatchEndToEndTest.java
@@ -4,21 +4,23 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import me.ndPrince.routeOptimization.annotation.EndToEndTest;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.IOException;
-
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 @EndToEndTest
 class DispatchEndToEndTest {
 
     private RequestSpecification requestSpecification;
-    private String dispatchUrl = "localhost:8080/dispatch";
+    private String dispatchUrl = "http://localhost:8080/dispatch";
 
     @BeforeEach
     void setUp(){
@@ -26,16 +28,19 @@ class DispatchEndToEndTest {
     }
 
     @Test
-    void dispatchRequest_basic() throws IOException {
-        try{
-            Response response = requestSpecification
-                    .body(EndToEndTestUtil.normalRequest())
-                    .post(dispatchUrl);
-            response.then().assertThat().statusCode(200);
-        } finally {
+    void dispatchRequest_basic() throws JSONException {
+        Response response = requestSpecification
+                .body(EndToEndTestUtil.normalRequest())
+                .post(dispatchUrl);
+        response.then().assertThat().statusCode(200);
 
-        }
+        JSONObject jsonObject = new JSONObject(response.getBody().jsonPath().prettyPrint());
+        // JSONArray vehicleSchedules = jsonObject.getJSONArray("vehicleSchedules"); // can't extract from null
+        JSONArray dummyList = jsonObject.getJSONArray("dummyList");
+        String id = jsonObject.getString("id");
 
+        assertEquals(3, dummyList.length());
+        assertEquals("1", id);
     }
 
 }
